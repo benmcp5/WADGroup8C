6dc4b0aa9de1a24a23b03b83938c92df4dd66602
AdamW-GU <98156099+AdamW-GU@users.noreply.github.com>
2022-03-14 16:57:05 +0000

    Adam - URLS & views first commit (Don't Delete)

--- ClubScoutGlasgow/urls.py
+++ ClubScoutGlasgow/urls.py
@@ -1,32 +1,19 @@
-"""tango_with_django_project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
 from ClubScoutGlasgow import views
-
 
 app_name = 'ClubScoutGlasgow'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('club/<slug:club_name_slug>/',
-        views.show_club, name='show_club'),
-    path('register/', views.register, name='register'),
+    path('home/', views.home, name='home'),
+    path('about/', views.about, name="about"),
     path('login/', views.user_login, name='login'),
+    path('login/signup/', views.user_signup, name='signup'),
     path('logout/', views.user_logout, name='logout'),
+    path('map/', views.map, name='map'),
+    path('clubs/', views.clubs, name='clubs'),
+    path('clubs/request-a-club/', views.club_request, name='club_request'),
+    path('club/<slug:club_name_slug>/', views.show_club, name='show_club'),
+    path('club/<slug:club_name_slug>/write_review', views.write_review, name='write_review'),
     path('add_club/', views.add_club, name='add_club'),
-
-
 ]
--- ClubScoutGlasgow/views.py
+++ ClubScoutGlasgow/views.py
@@ -1,51 +1,81 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from ClubScoutGlasgow.models import UserProfile, Club, Review, login_required, staff_member_required
-from ClubScoutGlasgow.forms import UserForm, UserProfileForm, ClubForm
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
-from datetime import datetime
+from django.contrib.auth.decorators import login_required
+from ClubScoutGlasgow.models import UserProfile, Club, Review, login_required, staff_member_required
+from ClubScoutGlasgow.forms import UserForm, UserProfileForm, ClubForm
+# Create your views here.
 
 def home(request):
         context_dict = {}
-        clubs = Club.objects.order_by('-name')[:5]
+        clubs = Club.objects.order_by('-name')[:5] #to be replaced
         context_dict['clubs'] = clubs
         return render(request, 'ClubScoutGlasgow/home.html', context=context_dict)
+    
+def about(request):
+    return render(request, 'ClubScoutGlasgow/about.html')
+    
+def user_login(request): #copied from twd, maybe not compatible?
+    if request.method == 'POST':
 
-def register(request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('ClubScoutGlasgow:index'))
+            else:
+                return HttpResponse("Your account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request,'ClubScoutGlasgow/login.html')
+        
+def user_signup(request): #copied from twd
     registered = False
-
     if request.method == 'POST':
         user_form = UserForm(request.POST)
         profile_form = UserProfileForm(request.POST)
-                # If the two forms are valid...
-        if user_form.is_valid() and profile_form.is_valid(): # Save the user's form data to the database.
+        
+        if user_form.is_valid() and profile_form.is_valid():
             user = user_form.save()
+            
             user.set_password(user.password)
             user.save()
-
+            
             profile = profile_form.save(commit=False)
             profile.user = user
-            # Did the user provide a profile picture?
-            # If so, we need to get it from the input form and #put it in the UserProfile model.
-
-            # Now we save the UserProfile model instance.
+            
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+                
             profile.save()
-            # Update our variable to indicate that the template # registration was successful.
+            
             registered = True
         else:
-
             print(user_form.errors, profile_form.errors)
     else:
         user_form = UserForm()
         profile_form = UserProfileForm()
-            # Render the template depending on the context.
-    return render(request, 'ClubScoutGlasgow/register.html',
-                      context = {'user_form': user_form,
-                                 'profile_form': profile_form,
-                                 'registered': registered})
+    
+    return render(request, 'ClubScoutGlasgow/register.html', context = {'user_form': user_form,'profile_form': profile_form, 'registered': registered})
 
+def map(request):
+    return render(request, 'ClubScoutGlasgow/map.html')
+
+def clubs(request, club_name_slug): #yet to add sorting options?
+    context_dict = {}
+    try:
+        club = Clubs.objects.order_by('name')
+        context_dict['club'] = club
+    except Clubs.DoesNotExist:
+        context_dict['club'] = None
+    return render(request, 'ClubScoutGlasgow/clubs.html', context=context_dict) #note clubs.html for whole list template, not club.html for individual
 
 def show_club(request, club_name_slug):
     context_dict={}
@@ -58,46 +88,37 @@ def show_club(request, club_name_slug):
 
     return render(request, 'ClubScoutGlasgow/club.html', context = context_dict)
 
-def user_login(request):
-    # If the request is a HTTP POST, try to pull out the relevant information.
-    if request.method == 'POST':
-            # Gather the username and password provided by the user.
-            # This information is obtained from the login form.
-            # We use request.POST.get('<variable>') as opposed
-            # to request.POST['<variable>'], because the
-        # request.POST.get('<variable>') returns None if the
-        # value does not exist, while request.POST['<variable>'] # will raise a KeyError exception.
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        # Use Django's machinery to attempt to see if the username/password # combination is valid - a User object is returned if it is.
-        user = authenticate(username=username, password=password)
-        # If we have a User object, the details are correct.
-        # If None (Python's way of representing the absence of a value), no user # with matching credentials was found.
-        if user:
-            # Is the account active? It could have been disabled.
-            if user.is_active:
-                # If the account is valid and active, we can log the user in. # We'll send the user back to the homepage.
-                login(request, user)
-                return redirect(reverse('ClubScoutGlasgow:home'))
-            else:
-            # An inactive account was used - no logging in!
-                return HttpResponse("Your ClubScoutGlasgow account is disabled.")
-        else:
-            # Bad login details were provided. So we can't log the user in.
-            print(f"Invalid login details: {username}, {password}")
-            return render(request, 'ClubScoutGlasgow/login.html')
-    else:
-        # No context variables to pass to the template system, hence the
-        # blank dictionary object...
-        return render(request, 'ClubScoutGlasgow/login.html')
-
-
 @login_required
 def user_logout(request):
     # Since we know the user is logged in, we can now just log them out.
     logout(request)
     # Take the user back to the homepage.
     return redirect(reverse('ClubScoutGlasgow:home'))
+
+def club_request(request):
+    return render(request, 'ClubScoutGlasgow/club_request.html')
+
+def write_review(request, club_name_slug): #no ReviewForm yet
+    context_dict = {}
+    try:
+        club = Club.objects.get(slug = club_name_slug)
+    except Club.DoesNotExist:
+        club = None
+    if club == None:
+        return redirect('ClubScoutGlasgow/')
+    
+    form = ReviewForm()
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+
+            print("-BCBCVJBCVJC")
+            return redirect('/ClubScoutGlasgow/')
+    else:
+        print(form.errors)
+    return render(request, 'ClubScoutGlasgow/write_review.html', context = context_dict)
 
 @staff_member_required
 def add_club(request):
@@ -120,3 +141,5 @@ def add_club(request):
         print(form.errors)
         # Will handle the bad form, new form, or no form supplied cases. # Render the form with error messages (if any).
     return render(request, 'ClubScoutGlasgow/add_club.html', {'form': form})
+
+
