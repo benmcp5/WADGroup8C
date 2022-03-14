1b307b30e80f13e325fbd4a52ca01f594a8d2864
Max Bell <2604835b@student.gla.ac.uk>
2022-03-14 15:29:15 +0000

    added non-working population script

---
+++ population_script.py
@@ -0,0 +1,10 @@
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WADGroup8C.settings')
+
+# NOT OPERATIONAL
+
+import django
+
+django.setup()
+# from ClubScoutGlasgow.models import Category, Page
