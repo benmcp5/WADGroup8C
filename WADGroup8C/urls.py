d7e2829fc9079076b3f001b07a7b106988e18559
Adam Wong <2549779W@student.gla.ac.uk>
2022-03-26 14:50:02 +0000

    serve media files line

--- WADGroup8C/urls.py
+++ WADGroup8C/urls.py
@@ -25,3 +25,4 @@ urlpatterns = [
     path('ClubScoutGlasgow/', include('ClubScoutGlasgow.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
