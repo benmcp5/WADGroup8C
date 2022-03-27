22516d81f6df51708680ef959b44478c306e6d3f
Ben McPartlin <2560937m@student.gla.ac.uk>
2022-03-27 20:59:03 +0100

    Ben- more tests written

--- static/javascript/CSG.js
+++ static/javascript/CSG.js
@@ -23,6 +23,17 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
 }
 
-function onLoad(){
-  plusSlides(1);
+
+function outputStars(div, rating ){
+  var stars = div
+  var star=[5];
+  for(let i = 0; i < 5;i++){
+      star[i] = document.createElement("span");
+      if(i<rating){
+          star[i].className = 'fa fa-star checked';
+      }else{
+          star[i].className = 'fa fa-star';
+      }
+      stars.appendChild(star[i]);
+  }
 }
